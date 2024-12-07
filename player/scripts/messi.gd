ddaa19060a6a6e5bd374c36019cc2896903d70c9
Florencia Morelli <florenciamorelliIT@gmail.com>
2024-12-07 20:19:45 -0300

    feature(player): 4-way movement implemented

--- player/scripts/messi.gd
+++ player/scripts/messi.gd
@@ -1,15 +1,23 @@
 extends CharacterBody2D
 
-@export var speed = 250
-var motion = Vector2()
+# speed in pixels/sec
+var speed = 300 
 
 func _physics_process(delta):
+	#	Get direction of movement
+	var direction = Input.get_vector("left", "right", "up", "down")
 
-	if Input.is_action_pressed("ui_left"):
-		motion.x = -speed
-	if Input.is_action_pressed("ui_right"):
-		motion.x = speed
-	if Input.is_action_pressed("ui_up"):
-		motion.y = -speed
-	if Input.is_action_pressed("ui_down"):
-		motion.y = speed
+	# Stop diagonal movement by listening for input then setting axis to zero
+	if Input.is_action_pressed("right") || Input.is_action_pressed("left"):
+		direction.y = 0
+	elif Input.is_action_pressed("up") || Input.is_action_pressed("down"):
+		direction.x = 0
+	else:
+		direction = Vector2.ZERO
+	
+	#	Normalize directional movement
+	direction = direction.normalized()
+	
+	#	Setup the movement
+	velocity = (direction * speed)
+	move_and_slide()
