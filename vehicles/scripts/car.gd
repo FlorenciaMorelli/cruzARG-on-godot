729f25cd5ab903cc11c016aa5180e70a8fc52673
Florencia Morelli <florenciamorelliIT@gmail.com>
2024-12-14 15:33:25 -0300

    feature(vehicles): implemented movement to the left with changes on sprite and loop

--- vehicles/scripts/car.gd
+++ vehicles/scripts/car.gd
@@ -1,17 +1,32 @@
 extends RigidBody2D
 
-# Speed in pixels/sec
-@export var speed = 5
+#	Speed in pixels/sec
+@export var speed = 300
+#	Direction of movement
+@export var direction: int = 1 # 1 goes right, -1 goes left
+#	Sprite Node for changing visual direction
+@onready var sprite: Sprite2D = $Sprite2D
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	_update_sprite_direction()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	move_and_collide(Vector2(speed, 0))
+	linear_velocity = Vector2(speed * direction, 0)
+
+
+func _update_sprite_direction() -> void:
+	if direction == 1:
+		sprite.flip_h = false # Dirección hacia la derecha
+	elif direction == -1:
+		sprite.flip_h = true # Dirección hacia la izquierda
 
 
 func _on_screen_exited() -> void:
-	global_position.x = get_parent().global_position.x
+	if direction == 1:
+		global_position.x = get_parent().global_position.x
+	elif direction == -1:
+		global_position.x = get_viewport_rect().size.x
