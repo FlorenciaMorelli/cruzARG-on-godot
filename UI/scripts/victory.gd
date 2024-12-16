da2da79920d137301c678a6eb5045f6eb30fd0c6
Florencia Morelli <florenciamorelliIT@gmail.com>
2024-12-15 21:22:36 -0300

    feature(sounds): added city sounds, background music and victory / game over sound effects

--- UI/scripts/victory.gd
+++ UI/scripts/victory.gd
@@ -7,6 +7,7 @@ var record_time: float = 0.0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	set_times()
+	$Victory_AudioStreamPlayer2D.play()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
