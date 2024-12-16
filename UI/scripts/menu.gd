1d01f2854841a4a68c68e9d3b4af1164c05c2884
Florencia Morelli <florenciamorelliIT@gmail.com>
2024-12-16 17:20:45 -0300

    feature(menu): implemented key control

--- UI/scripts/menu.gd
+++ UI/scripts/menu.gd
@@ -1,13 +1,46 @@
 extends CanvasLayer
 
+# Store the selected index
+var selected_index: int = 0
+@onready var play_button : Button = $Menu/VerticalContainer/Play
+@onready var howtoplay_button : Button = $Menu/VerticalContainer/HowToPlay
+@onready var exit_button : Button = $Menu/VerticalContainer/Exit
+var menu_items = []
+
+# Speed for selecting menu items
+var move_speed = 1
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	menu_items = [play_button, howtoplay_button, exit_button]
+	# Set the initial selected item
+	update_selected_item()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("down"):
+		selected_index += move_speed
+		selected_index = clamp(selected_index, 0, len(menu_items) - 1)
+		update_selected_item()
+		
+	if Input.is_action_just_pressed("up"):
+		selected_index -= move_speed
+		selected_index = clamp(selected_index, 0, len(menu_items) - 1)
+		update_selected_item()
+	
+	if Input.is_action_just_pressed("select") : 
+		menu_items[selected_index].emit_signal("pressed")
+	
+	if Input.is_action_just_pressed("exit") : 
+		get_tree().quit()
+
+func update_selected_item():
+	# Deselect all menu items
+	for i in range(len(menu_items)):
+		menu_items[i].modulate = Color(0.0, 0.0, 0.0, 1.0)  # Normal color
+	# Select the current item
+	menu_items[selected_index].modulate = Color(0.647059, 0.164706, 0.164706, 1)  # Highlight color
 
 
 func _on_play_pressed() -> void:
