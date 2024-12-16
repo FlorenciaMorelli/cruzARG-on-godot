73f22619cb725f03fbf040ad945c6f19f36efe12
Florencia Morelli <florenciamorelliIT@gmail.com>
2024-12-16 11:40:59 -0300

    style(victory): modified time and record layout

--- UI/scripts/victory.gd
+++ UI/scripts/victory.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
-@onready var time_label: Label = $Victory_CanvasLayer/Victory_Control/Victory_VBoxContainer/TimeLabel
-@onready var record_label: Label = $Victory_CanvasLayer/Victory_Control/Victory_VBoxContainer/RecordLabel
+@onready var time_label: Label = $Victory_CanvasLayer/HBoxContainer/TimeLabel
+@onready var record_label: Label = $Victory_CanvasLayer/HBoxContainer/RecordLabel
 var record_time: float = 0.0
 
 # Called when the node enters the scene tree for the first time.
@@ -25,9 +25,10 @@ func set_times():
 	if record_time == 0 or final_time < record_time:
 		DataManager.record_time = final_time
 		DataManager.save_data()
-		record_label.text = "New Record! %.2f s" % final_time
+		record_label.modulate = Color(1.0, 0.0, 0.0, 1.0)
+		record_label.text = "Nuevo Récord! %.2f s" % final_time
 	else:
-		record_label.text = "Record: %.2f s" % record_time
+		record_label.text = "Récord: %.2f s" % record_time
 
 	# Mostrar el tiempo final
-	time_label.text = "Your Time: %.2f s" % final_time
+	time_label.text = "Tiempo: %.2f s" % final_time
